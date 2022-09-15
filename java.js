506e493f7e57aff9be232c2761ed3a79e8695ac4
juliocesar-ramirez <julio.ramirez@pucp.pe>
2022-09-14 20:09:29 -0500

    zoom inicio

--- java.js
+++ java.js
@@ -15,6 +15,7 @@ var a3 = document.getElementById("a3");
 var a4 = document.getElementById("a4");
 
 var container = document.querySelector(".container");
+var fondo = document.querySelector(".fondo");
 
 b1.addEventListener("click", funcion1);
 b2.addEventListener("click", funcion2);
@@ -22,36 +23,55 @@ b3.addEventListener("click", funcion3);
 b4.addEventListener("click", funcion4);
 
 b1x.addEventListener("click", function () {
-    setTimeout(funcion1x,325)});
-b2x.addEventListener("click", funcion2x);
-b3x.addEventListener("click", funcion3x);
-b4x.addEventListener("click", funcion4x);
+	a1.classList.add("apagar");
+	setTimeout(funcion1x, 326);
+});
+b2x.addEventListener("click", function () {
+    a2.classList.add("apagar");
+	setTimeout(funcion2x, 326);
+});
+b3x.addEventListener("click", function () {
+	a3.classList.add("apagar");
+	setTimeout(funcion3x, 326);
+});
+b4x.addEventListener("click", function () {
+	a4.classList.add("apagar");
+	setTimeout(funcion4x, 326);
+});
 
 function funcion1() {
+	a1.classList.remove("apagar");
 	a1.classList.add("active");
 	container.classList.add("desactive");
 	a1.classList.add("prender");
 }
 
 function funcion2() {
+	a2.classList.remove("apagar");
 	a2.classList.add("active");
 	container.classList.add("desactive");
+	a2.classList.add("prender");
 }
 
 function funcion3() {
+	a3.classList.remove("apagar");
 	a3.classList.add("active");
-	container.classList.add("desactive");
+    container.classList.add("desactive");
+    a3.classList.add("prender");
 }
 
-
 function funcion4() {
+	a4.classList.remove("apagar");
 	a4.classList.add("active");
-	container.classList.add("desactive");
+    container.classList.add("desactive");
+	a3.classList.add("prender");
 }
 
 function funcion1x() {
 	a1.classList.remove("active");
 	container.classList.remove("desactive");
+	fondo.classList.add("zoom");
+	fondo.classList.add("zoomf");
 }
 
 function funcion2x() {
